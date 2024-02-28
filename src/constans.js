165f99ecb93b1f70894d615b19e97c1ebb192ac9
KacJL <49stay.wild49@gmail.com>
2024-02-28 14:43:34 +0300

    update file main.js

--- src/constans.js
+++ src/constans.js
@@ -1,6 +1,6 @@
 import md5 from "md5";
 
-export const URL_API = "http://api.valantis.store:40000/"
+export const URL_API = "https://api.valantis.store:41000/"
 export const API_PASSWORD = "Valantis"
 export const timestamp = new Date().toISOString().slice(0, 10).replace(/-/g, '');
 export const authString = md5(`${API_PASSWORD}_${timestamp}`);
